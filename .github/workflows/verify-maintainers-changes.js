eb153571d7346b1a3545f4efd2b161039ab696a8
14Richa <onlinericha19@gmail.com>
2023-06-26 23:45:45 +0530

    added

--- .github/workflows/verify-maintainers-changes.js
+++ .github/workflows/verify-maintainers-changes.js
@@ -1,11 +1,8 @@
 const { execSync } = require("child_process");
+const yaml = require("js-yaml");
+const fs = require("fs");
 
 const prAuthor = process.argv[2];
-
-// Get the differences in Maintainers.yaml
-const diffOutput = execSync(
-  "git diff origin/main --unified=0 -- Maintainers.yaml"
-).toString();
 
 // Check if the PR is made by a async-bot
 if (prAuthor.includes("[async-bot]")) {
@@ -13,15 +10,43 @@ if (prAuthor.includes("[async-bot]")) {
   process.exit(0);
 }
 
-// Check if there are critical changes
-const criticalChanges = ["github:", "repos:"];
+// Fetch the Maintainers.yaml file before changes
+execSync("git fetch origin main");
+execSync("git checkout FETCH_HEAD -- Maintainers.yaml");
+const maintainersBefore = yaml.load(
+  fs.readFileSync("Maintainers.yaml", "utf8")
+);
 
-for (const criticalChange of criticalChanges) {
-  if (diffOutput.includes(criticalChange)) {
-    console.error(
-      `::error::Critical changes have been made to Maintainers.yaml by ${prAuthor}. Please review.`
-    );
-    process.exit(1);
+// Reset to the current state
+execSync("git checkout HEAD Maintainers.yaml");
+const maintainersAfter = yaml.load(fs.readFileSync("Maintainers.yaml", "utf8"));
+
+// Compare repos arrays for each maintainer
+const beforeRepos = maintainersBefore.map(
+  (maintainer) => maintainer.repos || []
+);
+const afterRepos = maintainersAfter.map((maintainer) => maintainer.repos || []);
+
+for (let i = 0; i < beforeRepos.length; i++) {
+  const beforeSet = new Set(beforeRepos[i]);
+  const afterSet = new Set(afterRepos[i] || []);
+
+  for (const repo of beforeRepos[i]) {
+    if (!afterSet.has(repo)) {
+      console.error(
+        `::error::Critical changes have been made to Maintainers.yaml by ${prAuthor}. A repo has been removed. Please review.`
+      );
+      process.exit(1);
+    }
+  }
+
+  for (const repo of afterRepos[i] || []) {
+    if (!beforeSet.has(repo)) {
+      console.error(
+        `::error::Critical changes have been made to Maintainers.yaml by ${prAuthor}. A repo has been added. Please review.`
+      );
+      process.exit(1);
+    }
   }
 }
 
