b21708e8092841b6912367783c2f80f9786a7afc
14Richa <onlinericha19@gmail.com>
2023-06-26 23:16:58 +0530

    updated

--- .github/workflows/verify-maintainers-changes.js
+++ .github/workflows/verify-maintainers-changes.js
@@ -1,37 +1,28 @@
-const yaml = require("js-yaml");
-const fs = require("fs");
+const { execSync } = require("child_process");
 
-try {
-  // Read the Maintainers.yaml file
-  const maintainersData = yaml.load(
-    fs.readFileSync("Maintainers.yaml", "utf8")
-  );
+const prAuthor = process.argv[2];
 
-  // Check if changes were made by a bot
-  if (maintainersData.updated_by_bot) {
-    console.log("Changes made by a bot. Skipping verification.");
-    process.exit(0); // Exit with success status code
-  }
+// Get the differences in Maintainers.yaml
+const diffOutput = execSync(
+  "git diff origin/main --unified=0 -- Maintainers.yaml"
+).toString();
 
-  // Check for critical attribute modifications
-  const modifiedMaintainers = maintainersData.maintainers.filter(
-    (maintainer) => maintainer.updated
-  );
-  const hasCriticalAttributeModifications = modifiedMaintainers.some(
-    (maintainer) => maintainer.github || maintainer.repo || maintainer.removed
-  );
-
-  if (hasCriticalAttributeModifications) {
-    console.error("Invalid changes detected in the Maintainers.yaml file.");
-    console.error(
-      "Please ensure that critical attributes are not modified manually."
-    );
-    process.exit(1); // Exit with failure status code
-  }
-
-  // If no issues were found, exit with success status code
+// Check if the PR is made by a async-bot
+if (prAuthor.includes("[async-bot]")) {
+  console.log("Changes made by a async-bot. Verification not necessary.");
   process.exit(0);
-} catch (error) {
-  console.error("An error occurred during verification:", error);
-  process.exit(1); // Exit with failure status code
 }
+
+// Check if there are critical changes
+const criticalChanges = ["github:", "repos:"];
+
+for (const criticalChange of criticalChanges) {
+  if (diffOutput.includes(criticalChange)) {
+    console.error(
+      `::error::Critical changes have been made to Maintainers.yaml by ${prAuthor}. Please review.`
+    );
+    process.exit(1);
+  }
+}
+
+console.log("No critical changes detected.");
