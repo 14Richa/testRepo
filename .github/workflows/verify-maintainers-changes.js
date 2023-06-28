ea7df651f26535e8b4b1826a913cea6fea22490f
14Richa <onlinericha19@gmail.com>
2023-06-28 20:09:42 +0530

    minor fix

--- .github/workflows/verify-maintainers-changes.js
+++ .github/workflows/verify-maintainers-changes.js
@@ -1,151 +1,52 @@
-// const { execSync } = require("child_process");
-// const yaml = require("yaml");
-// const fs = require("fs");
-
-// const prAuthor = process.argv[2];
-
-// // Check if the PR is made by an async-bot
-// if (prAuthor.includes("[async-bot]")) {
-//   console.log("Changes made by an async-bot. Verification not necessary.");
-//   process.exit(0);
-// }
-
-// // Fetch the Maintainers.yaml file before changes
-// execSync("git fetch origin main");
-// execSync("git checkout FETCH_HEAD -- Maintainers.yaml");
-// const maintainersBefore = yaml.parse(
-//   fs.readFileSync("Maintainers.yaml", "utf8")
-// );
-
-// // Reset to the current state
-// execSync("git checkout HEAD Maintainers.yaml");
-// const maintainersAfter = yaml.parse(
-//   fs.readFileSync("Maintainers.yaml", "utf8")
-// );
-
-// // Check if any new maintainer objects are added
-// const beforeSet = new Set(
-//   maintainersBefore.map((maintainer) => maintainer.name)
-// );
-// const afterSet = new Set(maintainersAfter.map((maintainer) => maintainer.name));
-
-// for (const maintainer of maintainersAfter) {
-//   if (!beforeSet.has(maintainer.name)) {
-//     console.error(
-//       `::error::Critical changes have been made to Maintainers.yaml by ${prAuthor}. A new maintainer object has been added. Please review.`
-//     );
-//     process.exit(1);
-//   }
-// }
-
-// // Check if any maintainer objects are removed
-// for (const maintainer of maintainersBefore) {
-//   if (!afterSet.has(maintainer.name)) {
-//     console.error(
-//       `::error::Critical changes have been made to Maintainers.yaml by ${prAuthor}. A maintainer object has been removed. Please review.`
-//     );
-//     process.exit(1);
-//   }
-// }
-
-// // Check for critical attribute changes
-// for (let i = 0; i < maintainersAfter.length; i++) {
-//   const maintainerBefore = maintainersBefore.find(
-//     (m) => m.name === maintainersAfter[i].name
-//   );
-//   const maintainerAfter = maintainersAfter[i];
-
-//   if (
-//     maintainerBefore &&
-//     (maintainerBefore.github !== maintainerAfter.github ||
-//       !arraysAreEqual(maintainerBefore.repos, maintainerAfter.repos))
-//   ) {
-//     console.error(
-//       `::error::Critical changes have been made to Maintainers.yaml by ${prAuthor}. Changes to critical attributes detected. Please review.`
-//     );
-//     process.exit(1);
-//   }
-// }
-
-// console.log("No critical changes detected.");
-
-// // Helper function to check if two arrays are equal
-// function arraysAreEqual(arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-
-//   return true;
-// }
-
-
-const { execSync } = require("child_process");
 const yaml = require("yaml");
 const fs = require("fs");
 
-const prAuthor = process.argv[2];
-
-if (prAuthor.includes("[async-bot]")) {
-  console.log("Changes made by an async-bot. Verification not necessary.");
-  process.exit(0);
-}
-
-execSync("git fetch origin main");
-execSync("git checkout FETCH_HEAD -- Maintainers.yaml");
-
-const maintainersBefore = yaml.parse(fs.readFileSync("Maintainers.yaml", "utf8"));
-
-execSync("git checkout HEAD Maintainers.yaml");
-const maintainersAfter = yaml.parse(fs.readFileSync("Maintainers.yaml", "utf8"));
-
-const beforeSet = new Set(maintainersBefore.map((maintainer) => maintainer.name));
-const afterSet = new Set(maintainersAfter.map((maintainer) => maintainer.name));
-
-let isCriticalChangeDetected = false;
-
-for (const maintainer of maintainersAfter) {
-  if (!beforeSet.has(maintainer.name)) {
-    console.error(`::error::Critical changes have been made to Maintainers.yaml by ${prAuthor}. A new maintainer object has been added. Please review.`);
-    isCriticalChangeDetected = true;
-    break;
+function verifyMaintainersChanges(prAuthor, mainPath, prPath) {
+  if (prAuthor.includes("[async-bot]")) {
+    console.log("Changes made by an async-bot. Verification not necessary.");
+    return { isCriticalChange: false };
   }
-}
 
-if (!isCriticalChangeDetected) {
-  for (const maintainer of maintainersBefore) {
-    if (!afterSet.has(maintainer.name)) {
-      console.error(`::error::Critical changes have been made to Maintainers.yaml by ${prAuthor}. A maintainer object has been removed. Please review.`);
-      isCriticalChangeDetected = true;
-      break;
-    }
-  }
-}
+  const maintainersBefore = yaml.parse(fs.readFileSync(prPath, "utf8"));
+  const maintainersAfter = yaml.parse(fs.readFileSync(mainPath, "utf8"));
 
-if (!isCriticalChangeDetected) {
-  for (let i = 0; i < maintainersAfter.length; i++) {
-    const maintainerBefore = maintainersBefore.find(m => m.name === maintainersAfter[i].name);
-    const maintainerAfter = maintainersAfter[i];
+  const beforeSet = new Set(maintainersBefore.map((maintainer) => maintainer.name));
+  const afterSet = new Set(maintainersAfter.map((maintainer) => maintainer.name));
 
-    if (maintainerBefore && (maintainerBefore.github !== maintainerAfter.github || !arraysAreEqual(maintainerBefore.repos, maintainerAfter.repos))) {
-      console.error(`::error::Critical changes have been made to Maintainers.yaml by ${prAuthor}. Changes to critical attributes detected. Please review.`);
+  let isCriticalChangeDetected = false;
+
+  for (const maintainer of maintainersAfter) {
+    if (!beforeSet.has(maintainer.name)) {
+      console.error(`::error::Critical changes have been made to Maintainers.yaml by ${prAuthor}. A new maintainer object has been added. Please review.`);
       isCriticalChangeDetected = true;
       break;
     }
   }
 
   if (!isCriticalChangeDetected) {
-    console.log("No critical changes detected.");
+    for (const maintainer of maintainersBefore) {
+      if (!afterSet.has(maintainer.name)) {
+        console.error(`::error::Critical changes have been made to Maintainers.yaml by ${prAuthor}. A maintainer object has been removed. Please review.`);
+        isCriticalChangeDetected = true;
+        break;
+      }
+    }
   }
-}
 
-if (isCriticalChangeDetected) {
-    console.log("::set-output name=isCriticalChange::true");
+  if (!isCriticalChangeDetected) {
+    for (let i = 0; i < maintainersAfter.length; i++) {
+      const maintainerBefore = maintainersBefore.find(m => m.name === maintainersAfter[i].name);
+      const maintainerAfter = maintainersAfter[i];
+
+      if (maintainerBefore && (maintainerBefore.github !== maintainerAfter.github || !arraysAreEqual(maintainerBefore.repos, maintainerAfter.repos))) {
+        console.error(`::error::Critical changes have been made to Maintainers.yaml by ${prAuthor}. Changes to critical attributes detected. Please review.`);
+        isCriticalChangeDetected = true;
+        break;
+      }
+    }
+  }
+
+  return { isCriticalChange: isCriticalChangeDetected };
 }
 
 function arraysAreEqual(arr1, arr2) {
@@ -159,3 +60,5 @@ function arraysAreEqual(arr1, arr2) {
   }
   return true;
 }
+
+module.exports = verifyMaintainersChanges;
